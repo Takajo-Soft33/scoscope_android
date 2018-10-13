522a2730af8ffcef7e8aa3e569312dc35d60fd59
saida <saida_service@outlook.jp>
2018-10-13 21:37:11 +0900

    方向案内の画面遷移

---
+++ app/src/main/java/jp/procon29/scoscope/scoscope_androidapp/DirectionGuide.java
@@ -0,0 +1,12 @@
+package jp.procon29.scoscope.scoscope_androidapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DirectionGuide extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.direction_guide);
+    }
+}
