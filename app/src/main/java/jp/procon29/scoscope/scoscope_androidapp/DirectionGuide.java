920eba23254598a1726e8dddd4b9f3337207aaa7
saida <saida_service@outlook.jp>
2018-10-14 16:55:48 +0900

    リストに仮データの追加

--- app/src/main/java/jp/procon29/scoscope/scoscope_androidapp/DirectionGuide.java
+++ app/src/main/java/jp/procon29/scoscope/scoscope_androidapp/DirectionGuide.java
@@ -3,6 +3,10 @@ package jp.procon29.scoscope.scoscope_androidapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DirectionGuide extends AppCompatActivity {
     @Override
@@ -10,5 +14,14 @@ public class DirectionGuide extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.direction_guide);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ListView listView = (ListView) findViewById(R.id.searchList);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("徳山工業高等専門学校");
+        list.add("徳山工業高等専門学校 高城寮");
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+
+        listView.setAdapter(adapter);
     }
 }
