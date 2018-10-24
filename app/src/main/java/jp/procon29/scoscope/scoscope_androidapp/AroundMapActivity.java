86917f1e70e12afe0ad198690c5f768689e142b1
tct-i15fujimura1s <fujimura1s.i15.tct@gmail.com>
2018-10-24 12:57:32 +0900

    Add map feature; it doesn't include API KEY

--- app/src/main/java/jp/procon29/scoscope/scoscope_androidapp/AroundMapActivity.java
+++ app/src/main/java/jp/procon29/scoscope/scoscope_androidapp/AroundMapActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
 public class AroundMapActivity extends AppCompatActivity {
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +14,27 @@ public class AroundMapActivity extends AppCompatActivity {
 
         setContentView(R.layout.around_map);
 
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+
+        try {
+            googleMap = mapFragment.getMap();
+
+            if(savedInstanceState == null) {
+                mapFragment.setRetainInstance(true);
+
+                mapInit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void mapInit() {
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.setMyLocationEnabled(true);
+
+        CameraPosition cameraPos = new CameraPosition.Builder().target(new LatLng(35.681382, 139.766084)).zoom(15.5f).build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPos));
     }
 }
